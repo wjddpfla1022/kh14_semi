197b78015b07d7d70c5f8cef6e911e3ec2f533e2
ghodome <vjaewook@gmail.com>
2024-08-13 17:07:16 +0900

    oneTrillionCompany Started
    세미 프로젝트 파일 생성

---
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/OneTrillionCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.kh.oneTrillionCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTrillionCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTrillionCompanyApplication.class, args);
+	}
+
+}
---
+++ oneTrillionCompany/src/test/java/com/kh/semi/OneTrillionCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.semi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTrillionCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
