7739fdc2f5723236d20775cea327d544333b99d6
ChaeSuJin <univer7519@naver.com>
2024-08-22 15:32:09 +0900

    상품 컨트롤러

--- oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/CartController.java
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/CartController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.kh.oneTrillionCompany.dao.ItemDao;
 
 @Controller
-@RequestMapping("/cart")
+@RequestMapping("/")
 public class CartController {
 	
 	@Autowired
