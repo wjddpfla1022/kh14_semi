1f69c2715867580ec8eab93d489911fe0d0b30ea
minseokOo <minseok916@gmail.com>
2024-08-23 18:20:17 +0900

    Create delete-table.sql
    순서 정리된 테이블 삭제 구문입니다

---
+++ DB/delete-table.sql
@@ -0,0 +1,52 @@
+-- Review Image 테이블은 마지막에 삭제합니다.
+DROP TABLE review_image;
+
+-- Review 테이블 삭제
+DROP TABLE review;
+
+-- Reply 테이블 삭제
+DROP TABLE reply;
+
+-- QNA 테이블 삭제
+DROP TABLE QNA;
+
+-- Image 테이블 삭제
+DROP TABLE image;
+
+-- Attach 테이블 삭제
+DROP TABLE attach;
+
+-- Order Detail 테이블 삭제
+DROP TABLE order_detail;
+
+-- Refund 테이블 삭제
+DROP TABLE refund;
+
+-- Orders 테이블 삭제
+DROP TABLE orders;
+
+-- Cart 테이블 삭제
+DROP TABLE cart;
+
+-- Item 테이블 삭제
+DROP TABLE item;
+
+-- Block 테이블 삭제
+DROP TABLE block;
+
+-- Member 테이블 삭제
+DROP TABLE member;
+
+-- Category 테이블 삭제
+DROP TABLE category;
+
+-- 시퀀스 삭제
+DROP SEQUENCE review_seq;
+DROP SEQUENCE qna_seq;
+DROP SEQUENCE attach_seq;
+DROP SEQUENCE order_detail_seq;
+DROP SEQUENCE cart_seq;
+DROP SEQUENCE item_seq;
+DROP SEQUENCE block_seq;
+DROP SEQUENCE category_seq;
+DROP SEQUENCE order_seq;
