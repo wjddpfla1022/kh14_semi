214f105faf1630053317d18c6d17eef1a3c7120f
wjddpfla1022 <wjddpfla98@naver.com>
2024-08-26 11:20:00 +0900

    수정된 테이블에 맞게 orderDetail 주문상세 Dto,Mapper 항목 추가

--- oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/mapper/OrderDetailMapper.java
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/mapper/OrderDetailMapper.java
@@ -16,8 +16,11 @@ public class OrderDetailMapper implements RowMapper<OrderDetailDto> {
 		OrderDetailDto orderDetailDto = new OrderDetailDto();
 		orderDetailDto.setOrderDetailNo(rs.getInt("order_detail_no"));
 		orderDetailDto.setOrderDetailOrderNo(rs.getInt("order_detail_order_no"));
+		orderDetailDto.setOrderDetailItemNo(rs.getInt("order_detail_item_no"));
+		orderDetailDto.setOrderDetailBuyer(rs.getString("order_detail_buyer"));
 		orderDetailDto.setOrderDetailPrice(rs.getInt("order_detail_price"));
 		orderDetailDto.setOrderDetailCnt(rs.getInt("order_detail_cnt"));
+		orderDetailDto.setOrderDetailStatus(rs.getString("order_detail_status"));
 		
 		return orderDetailDto;
 	}
