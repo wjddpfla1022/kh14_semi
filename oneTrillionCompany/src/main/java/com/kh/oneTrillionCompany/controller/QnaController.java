0cc739404e61905e3ac99bee890c7d457b2bf16d
wjddpfla1022 <wjddpfla98@naver.com>
2024-08-26 15:20:25 +0900

    list, detail 코드 추가

--- oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/QnaController.java
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/QnaController.java
@@ -1,7 +1,5 @@
 package com.kh.oneTrillionCompany.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.oneTrillionCompany.dao.QnaDao;
 import com.kh.oneTrillionCompany.dto.QnaDto;
+import com.kh.oneTrillionCompany.exception.TargetNotFoundException;
+import com.kh.oneTrillionCompany.vo.PageVO;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -43,7 +44,20 @@ public class QnaController {
 		return "redirect:list";
 	}
 	@RequestMapping("list")
-	public String list() {
+	public String list(@ModelAttribute("pageVO") PageVO pageVO, Model model) {
+		model.addAttribute("qnaList", qnaDao.selectListByPaging(pageVO));
+		int count = qnaDao.countByPaging(pageVO);
+		pageVO.setCount(count);
 	    return "/WEB-INF/views/qna/list.jsp";
+	}
+	
+	@RequestMapping("/detail")
+	public String detail(@RequestParam int qnaNo, Model model) {
+		QnaDto qnaDto = qnaDao.selectOne(qnaNo);
+		if(qnaDto==null) {
+			throw new TargetNotFoundException("존재하지 않는 글번호");
+		}
+		model.addAttribute("qnaDto", qnaDto);
+		return "/WEB-INF/views/qna/detail.jsp";
 	}
 }
