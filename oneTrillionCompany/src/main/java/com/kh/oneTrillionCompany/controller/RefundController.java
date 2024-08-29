ccd24b81e94b782627808b99c92b80bc41c6f981
wjddpfla1022 <wjddpfla98@naver.com>
2024-08-29 14:10:53 +0900

    환불 등록(신청), 마이페이지 디자인 수정

--- oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/RefundController.java
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/RefundController.java
@@ -2,13 +2,16 @@ package com.kh.oneTrillionCompany.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.oneTrillionCompany.dao.RefundDao;
 import com.kh.oneTrillionCompany.dto.RefundDto;
+import com.kh.oneTrillionCompany.exception.TargetNotFoundException;
 
 @Controller
 @RequestMapping("/refund")
@@ -18,7 +21,10 @@ public class RefundController {
 	private RefundDao refundDao;
 	
 	@GetMapping("/insert")
-	public String insert() {
+	public String insert(Model model, @RequestParam int refundOrderDetailNo) {
+		RefundDto refundDto = refundDao.selectOne(refundOrderDetailNo);
+		if(refundDto == null) throw new TargetNotFoundException("존재하지 않는 주문입니다");
+		model.addAttribute("refundDto", refundDto);
 		return "/WEB-INF/views/refund/insert.jsp";
 	}
 	@PostMapping("/insert")
