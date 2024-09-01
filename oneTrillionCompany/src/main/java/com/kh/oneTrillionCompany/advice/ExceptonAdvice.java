3f5e5451a401c7e76b7688172c937cc8395c87bc
ghodome <vjaewook@gmail.com>
2024-09-02 03:54:24 +0900

    예외처리 메세지 노출 수정

--- oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/advice/ExceptonAdvice.java
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/advice/ExceptonAdvice.java
@@ -1,6 +1,7 @@
 package com.kh.oneTrillionCompany.advice;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,7 +11,8 @@ import com.kh.oneTrillionCompany.exception.TargetNotFoundException;
 public class ExceptonAdvice {
 	//예외 처리기
 	@ExceptionHandler(TargetNotFoundException.class)
-	public String notFound() {
+	public String notFound(TargetNotFoundException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
 		return "/WEB-INF/views/error/notFound.jsp";
 	}
 }
--- oneTrillionCompany/src/main/webapp/WEB-INF/views/error/notFound.jsp
+++ oneTrillionCompany/src/main/webapp/WEB-INF/views/error/notFound.jsp
@@ -1,5 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <jsp:include page="/WEB-INF/views/template/header.jsp"></jsp:include>
-<h1>대상이 존재하지 않습니다</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+</head>
+<body>
+<div class="container w-1200" style="min-height:400px">
+    <p>${errorMessage}</p> 
+</div>
+</body>
+</html>
 <jsp:include page="/WEB-INF/views/template/footer.jsp"></jsp:include>
