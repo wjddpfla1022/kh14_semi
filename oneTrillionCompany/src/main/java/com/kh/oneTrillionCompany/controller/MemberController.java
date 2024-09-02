1e57fa1850e8db39a3051406defde36cef66cce1
minseokOo <minseok916@gmail.com>
2024-09-02 14:59:21 +0900

    로그인 시 비밀번호 오류 수정

--- oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/MemberController.java
+++ oneTrillionCompany/src/main/java/com/kh/oneTrillionCompany/controller/MemberController.java
@@ -64,7 +64,7 @@ public class MemberController {
 		session.setAttribute("createdUser", memberId);
 		session.setAttribute("createdLevel", memberDto.getMemberRank());
 		memberDao.updateMemberLogin(memberId);
-		
+		 
 		return "redirect:/";
 		
 	}
